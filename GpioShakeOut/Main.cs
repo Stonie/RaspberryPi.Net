55fec122d1434814015b740b534e61922acb7e38
DrivenLogic <mail@drivenlogic.com.au>
2012-08-28 13:53:51 +1000

    Adding a few more shakeout methods

--- GpioShakeOut/Main.cs
+++ GpioShakeOut/Main.cs
@@ -8,15 +8,45 @@ namespace GpioShakeOut
 	{
 		public static void Main (string[] args)
 		{
-			GPIOFile gpio = new GPIOFile(GPIO.GPIOPins.GPIO00);
-            
-			for (int i = 0; i < 10; i++) //flash pin 17, 5 times on & off (1 second each)
-            {
-				Thread.Sleep(500);
-            	gpio.Write(Convert.ToBoolean(i%2));
-				
-				Console.WriteLine("Setting pin 17 to {0}",i%2);
-            }
+            FlashLed();
+            //MaxToggleSpeed();
       	}
+
+        private static void FlashLed()
+        {
+            // set up which GPIO pin we will use
+            GPIOFile gpio = new GPIOFile(GPIO.GPIOPins.GPIO00);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Thread.Sleep(200);
+                gpio.Write(Convert.ToBoolean(i % 2)); // toggle pin
+                Console.WriteLine("Setting pin 17 to {0}", i % 2);
+            }
+        }
+
+        private static void MaxToggleSpeed()
+        {
+            // set up which GPIO pin we will use
+            GPIOFile gpio = new GPIOFile(GPIO.GPIOPins.GPIO04);
+
+            bool toggle = true;
+            while (true)
+            {
+                gpio.Write(toggle ^= true);
+            }
+        }
+
+        private static void MaxToggleSpeed()
+        {
+            // set up which GPIO pin we will use
+            GPIOFile gpio = new GPIOFile(GPIO.GPIOPins.GPIO18,);
+
+            bool toggle = true;
+            while (true)
+            {
+                gpio.Write(toggle ^= true);
+            }
+        }
 	}
 }
